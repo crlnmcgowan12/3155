3dec5b8d2c3fc254abc338cc07be1337eb7da862
carolinemcgowan <crlnmcgowan@gmail.com>
2025-10-27 22:29:52 -0400

    q

--- Assignment_5/api/dependencies/config.py
+++
@@ -1,6 +0,0 @@
-# Inside api/dependencies/config.py
-class conf:
-    # ... other lines ...
-    database = "sandwich_maker_api"
-    user = "root"
-    password = "Carnick12$"  # <-- REPLACE THIS with your password!
