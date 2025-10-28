cddd3641e4dabf4c15db6b7aa4883b71e2bce939
carolinemcgowan <crlnmcgowan@gmail.com>
2025-10-27 23:19:09 -0400

    Fixing error line

--- Assignment5/api/main.py
+++ Assignment5/api/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 
 from .models import models, schemas
-from controllers import orders, sandwiches, resources, recipes
+from .controllers import orders, sandwiches, resources, recipes # The dot makes it relative
 from .dependencies.database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
