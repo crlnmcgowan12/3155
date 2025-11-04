0a511c2544fe875d28555d4132a871b838962ab9
carolinemcgowan <crlnmcgowan@gmail.com>
2025-11-03 21:15:59 -0500

    fix

--- Assignment5/api/models.py
+++ Assignment5/api/models.py
@@ -2,7 +2,8 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from database import Base  # assuming database.py defines Base = declarative_base()
+from .database import Base
+
 
 class User(Base):
     __tablename__ = "users"
